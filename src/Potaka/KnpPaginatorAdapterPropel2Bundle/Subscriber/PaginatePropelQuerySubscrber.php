f6d4640d63e0ac56948f0083de16a825f3a0ac21
angelk <angel.koilov@gmail.com>
2015-12-30 23:35:12 +0200

    added order

--- src/Potaka/KnpPaginatorAdapterPropel2Bundle/Subscriber/PaginatePropelQuerySubscrber.php
+++ src/Potaka/KnpPaginatorAdapterPropel2Bundle/Subscriber/PaginatePropelQuerySubscrber.php
@@ -18,14 +18,28 @@ class PaginatePropelQuerySubscrber implements EventSubscriberInterface
     {
         $target = $event->target;
         if ($target instanceof ModelCriteria) {
+            $sortFieldParamName = $event->options['sortFieldParameterName'];
+            if (isset($_GET[$sortFieldParamName])) {
+                $direction = strtolower($_GET[$event->options['sortDirectionParameterName']]) === 'asc' ? 'asc' : 'desc';
+                $part = $_GET[$sortFieldParamName];
+
+                if (isset($event->options['sortFieldWhitelist'])) {
+                    if (!in_array($_GET[$sortFieldParamName], $event->options['sortFieldWhitelist'])) {
+                        throw new \UnexpectedValueException("Cannot sort by: [{$_GET[$sortFieldParamName]}] this field is not in whitelist");
+                    }
+                }
+                
+                $target->orderBy($part, $direction);
+            }
+            
             $event->count = $target->count();
             $limit = $event->getLimit();
             $offset = $event->getOffset();
             $target->limit($limit);
             $target->offset($offset);
             $items = $target->find();
-            $items->getData();
-            $event->items = $items->getData();
+            $itemsData = $items->getData();
+            $event->items = $itemsData;
             $event->stopPropagation();
         }
     }
